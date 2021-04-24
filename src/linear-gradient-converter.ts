a14ed883aed9e56d87476c057a5a6646ce2eb83d
linwrui <linwrui@163.com>
2021-04-24 14:19:07 +0800

    Update linear-gradient-converter.ts

--- src/linear-gradient-converter.ts
+++ src/linear-gradient-converter.ts
@@ -12,7 +12,7 @@ export interface LinearGradientConvertOptions {
    */
   angle?: number;
   /**
-   * 
+   * use for calc color stops
    *
    * @type {ColorTransformTargets}
    * @memberof LinearGradientConvertOptions
